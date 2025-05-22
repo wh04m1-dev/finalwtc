43ebe50212a1c1481fd2b96ac90468db39c40abc
Sothea <officalsengchanthea@gmail.com>
2025-05-22 10:36:15 +0700

    fix register

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|string|min:6',
             'role' => 'in:admin,user,organizer',
         ]);
 
